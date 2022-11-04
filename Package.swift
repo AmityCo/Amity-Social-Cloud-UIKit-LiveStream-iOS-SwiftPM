6b25f194f31d5a89c34b427aed7053345235b716
Zryte <>
2022-11-04 13:17:13 +0700

    update Package.swift for v2.29.0

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AmityUIKitLiveStream",
+    products: [
+        .library(
+            name: "AmityUIKitLiveStream",
+            targets: ["AmityUIKitLiveStream", "AmityLiveVideoBroadcastKit", "AmityVideoPlayerKit", "MobileVLCKit"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AmityUIKitLiveStream",
+            url: "https://s3-ap-southeast-1.amazonaws.com/ekosdk-release/ios-uikit-frameworks/2.29.0/AmityUIKitLiveStream.xcframework.zip",
+            checksum: "ab239236a049d5b7327c2a977d50f396fa14e8cd4df21abb26a62cfc38689ed7"
+        ),
+        .binaryTarget(
+            name: "AmityLiveVideoBroadcastKit",
+            url: "https://s3-ap-southeast-1.amazonaws.com/ekosdk-release/ios-uikit-frameworks/2.29.0/AmityLiveVideoBroadcastKit.xcframework.zip",
+            checksum: "e72276257b73713e416050f7be90499b47696255cf06da3a9b1a1faad850196b"
+        ),
+        .binaryTarget(
+            name: "AmityVideoPlayerKit",
+            url: "https://s3-ap-southeast-1.amazonaws.com/ekosdk-release/ios-uikit-frameworks/2.29.0/AmityVideoPlayerKit.xcframework.zip",
+            checksum: "d5483bc21c26a528f0df135df930a8068d2415d5a8ed52be2471417bfa1fa080"
+        ),
+        .binaryTarget(
+            name: "MobileVLCKit",
+            url: "https://s3-ap-southeast-1.amazonaws.com/ekosdk-release/ios-frameworks/5.1.0/MobileVLCKit.xcframework.zip",
+            checksum: "64e78ecdf0657246ac047b995d86a890a1c78852be968f5d80de2b28c90dc1a9"
+        ),
+    ]
+)
